15d7ffb570d6f221eee48cbe7c2ca5c04f295f6a
ArtiFisher <artemis.fisher@gmail.com>
2020-10-16 19:30:31 +0300

    Add server and links

--- Module5/script.sh
+++ Module5/script.sh
@@ -15,6 +15,17 @@ sudo apt install nodejs
 aws s3 cp s3://artsiom-rybakou-task1/index.html /var/www/html/aws-hello.html
 aws s3 cp s3://artsiom-rybakou-task3 /var/www/html/csv/ --recursive
 bash /var/www/html/csv/script.sh
+service httpd start
+chkconfig httpd on
 
+
+
+cd Documents/Projects/AWS\ training/creds/
+cd /var/www/html/
 cat /var/log/cloud-init-output.log
 cat /var/www/html/aws-hello.html
+cat /var/www/html/csv/output.csv
+http://ec2-18-157-161-53.eu-central-1.compute.amazonaws.com/aws-hello.html
+
+
+http://module5-80031505.eu-central-1.elb.amazonaws.com/aws-hello.html
