5a44d9d5112a4085530a558946673a5f4d66b96a
ArtiFisher <artemis.fisher@gmail.com>
2020-10-19 20:30:11 +0300

    Add mysql and sequelize

--- Module8/index.js
+++ Module8/index.js
@@ -3,8 +3,14 @@ const path = require("path");
 const fs = require("fs");
 const AWS = require('aws-sdk');
 const express = require("express");
+const { Sequelize, QueryTypes } = require('sequelize');
 
 require('dotenv').config()
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_LOGIN, process.env.DB_PASSWORD, {
+  host     : process.env.DB_HOST,
+  dialect  : process.env.DB_DIALECT
+});
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -49,11 +55,13 @@ app.post(
             if (err) {
                 throw err;
             }
-            console.log(`File uploaded successfully. ${data.Location}`);
+            const currentDate = new Date().toLocaleString();
+            sequelize.query(`INSERT INTO images (name, upload_date) VALUES ("${req.file.originalname}", "${currentDate}");`,
+            { logging: console.log, type: QueryTypes.INSERT })
             res
                 .status(200)
                 .contentType("text/plain")
-                .end("File uploaded!");
+                .end(`File uploaded at ${currentDate}`);
         });
     } else {
       fs.unlink(tempPath, err => {
@@ -67,6 +75,18 @@ app.post(
     }
   }
 );
+
+app.get("/metadata", (req, res) => {
+    sequelize
+    .query(`SELECT * FROM images`, { logging: console.log, type: QueryTypes.SELECT })
+    .then(data => {
+      console.log(data)
+      res
+        .status(200)
+        .contentType("text/plain")
+        .end(data.toString());
+    })
+});
 
 app.get("/image", (req, res) => {
     s3.getObject({
